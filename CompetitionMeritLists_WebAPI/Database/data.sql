58ed9e3e536d176a25056b3549b4b991e981b919
Avneet Singh <avneetsingh2940@gmail.com>
2021-05-20 10:59:31 +1200

    Add project files.

---
+++ CompetitionMeritLists_WebAPI/Database/data.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[Students] ON
+INSERT INTO [dbo].[Students] ([Id], [Student_Name], [Student_age], [School_Name], [Subject_Name], [Subject_Code], [Marks_Obtained]) VALUES (1, N'Avneet Singh', 25, N'ATC', N'Programming', N'5898', N'96')
+INSERT INTO [dbo].[Students] ([Id], [Student_Name], [Student_age], [School_Name], [Subject_Name], [Subject_Code], [Marks_Obtained]) VALUES (2, N'Dilpreet Singh', 23, N'ATC', N'Database', N'5965', N'86')
+SET IDENTITY_INSERT [dbo].[Students] OFF
---
+++ CompetitionMeritLists_WebAPI/Migrations/20210513215720_Student_obj.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.IO;
+
+namespace CompetitionMeritLists_WebAPI.Migrations
+{
+    public partial class Student_obj : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Student_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Student_age = table.Column<int>(type: "int", nullable: false),
+                    School_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Subject_Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Subject_Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Marks_Obtained = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            var sqlFile = Path.Combine(".\\Database", @"data.sql");
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
